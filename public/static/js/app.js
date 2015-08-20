839aa41cb7a225a04dbb06c21f295db21169fdcc
George Rogers <grogers385@gmail.com>
2015-08-20 13:48:49 -0500

    update

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -2,7 +2,7 @@ require(["jquery", "underscore"], function($, _) {
   "use strict";
   var messages = [];
   var rescroll = function() {
-    var $msgs = $("#msgs-scroll");
+    var $msgs = $("#chat-scroll");
     $msgs.scrollTop($msgs.children().height());
   };
   var update = function(d) {
@@ -25,16 +25,16 @@ require(["jquery", "underscore"], function($, _) {
     rescroll();
   });
   $(function() {
-    $("form#send").on("submit", function(evt) {
+    $("form#chat-send").on("submit", function(evt) {
       evt.preventDefault();
       var self = this;
       var $self = $(self);
-      var $msg = $self.find("[name=msg]");
-      var $user = $self.find("[name=user]");
+      var $msg = $self.find("[name=message]");
+      var $user = $self.find("[name=name]");
       $.ajax({
         url: "/send",
         method: "POST",
-        data: {msg: $msg.val(), user: $user.val()}
+        data: {message: $msg.val(), name: $user.val()}
       }).success(function() {
         $msg.val("");
         rescroll();
--- templates/base.tpl
+++ templates/base.tpl
@@ -1,6 +1,8 @@
 <!doctype html>
 <html>
   <head>
+    <link href='http://fonts.googleapis.com/css?family=Inconsolata' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="/static/css/style.css">
     {{ template "head" . }} 
   </head>
   <body>
